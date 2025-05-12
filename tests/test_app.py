2ec3a8d707ba99ab9939dd156f4412e7315801e5
ihabe <ihabe.guenfoudi@student.uliege.be>
2025-05-12 07:10:27 +0200

    Fix pre-commit issues and add linting configurations

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,10 @@
-import sys
 import os
+import sys
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from api.flask.app import clean_tweet
+
 
 def test_clean_tweet():
     # Test URL removal
